2d833c0e852de177c3ba9bf293700e66f19c2d49
Harriet Ryder <harriethryder@gmail.com>
2018-07-05 07:25:02 +0100

    Add POST /users/{userId}/recordings endpoint

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'development';
 const ApiBuilder = require('claudia-api-builder');
 const mongoose = require('mongoose');
-const {Question} = require('./db/models');
+const { Question, Recording, User } = require('./db/models');
+const { badRequest } = require('./lib/response_helpers');
 
 const api = new ApiBuilder();
 
@@ -31,12 +32,43 @@ api.get('/languages/{language}/questions', (request) => {
   return Question.find({ language })
     .then(questions => {
       return new ApiBuilder.ApiResponse({ questions }, {
-        'Content-Types': 'application/json'
+        'Content-Type': 'application/json'
       });
     })
     .catch(err => {
       return new ApiBuilder.ApiResponse({ message: err }, 500);
     });
 });
+
+api.post('/users/{userId}/recordings', (request) => {
+  if (!request.body.language) return badRequest('language is required');
+  if (!request.body.lengthMillis) return badRequest('lengthMillis is required');
+  if (!request.body.url) return badRequest('url is required');
+  if (!request.body.questionId) return badRequest('questionId is required');
+
+  return User.findOne({id: request.pathParams.userId})
+    .then((user) => {
+      if (!user) return Promise.reject({
+        response: badRequest(`user ${request.pathParams.userId} does not exist`)
+      });
+      else return new Recording({
+        user_id: request.pathParams.userId,
+        language: request.body.language,
+        url: request.body.url,
+        length_millis: request.body.lengthMillis,
+        question_id: request.body.questionId,
+        created_at: new Date().getTime()
+      }).save();
+    })
+    .then(res => {
+      return new ApiBuilder.ApiResponse({ recording: res }, {
+        'Content-Type': 'application/json'
+      }, 201);
+    })
+    .catch(err => {
+      if (err.response) return err.response;
+      console.log(err);
+    });
+}, { cognitoAuthorizer: 'chachara-auth' });
 
 module.exports = api;
--- test/api.spec.js
+++ test/api.spec.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const { dropDb, seedDb } = require('../db/seeds');
 
 const questions = require('./data/questions.json');
+const users = require('./data/users.json');
 
 describe('api', () => {
   let lambdaContextSpy;
@@ -12,7 +13,7 @@ describe('api', () => {
       done: sinon.spy()
     };
 
-    return dropDb().then(() => seedDb(questions));
+    return dropDb().then(() => seedDb(questions, users));
   });
 
   afterAll(() => mongoose.disconnect());
@@ -31,6 +32,169 @@ describe('api', () => {
       }).then(done, done);
     });
   });
+
+  describe('POST /users/:userId/recordings', () => {
+    it('responds with 201 and the new recording if recording saved successfully', done => {
+      api.proxyRouter({
+        requestContext: {
+          resourcePath: '/users/{userId}/recordings',
+          httpMethod: 'POST'
+        },
+        pathParameters: {
+          userId: '123'
+        },
+        body: {
+          language: 'es',
+          url: 'http://www.foo.com',
+          lengthMillis: 100,
+          questionId: 1234
+        }
+      }, lambdaContextSpy).then(() => {
+        const [err, res] = lambdaContextSpy.done.firstCall.args;
+        
+        if (err) {
+          throw err;
+        }
+        
+        const data = JSON.parse(res.body);
+        expect(res.statusCode).toBe(201);
+        expect(data.recording.user_id).toBe('123');
+        expect(data.recording.length_millis).toBe(100);
+      }).then(done, done);
+    });
+    it('responds with 400 if url not provided in post body', done => {
+      api.proxyRouter({
+        requestContext: {
+          resourcePath: '/users/{userId}/recordings',
+          httpMethod: 'POST'
+        },
+        pathParameters: {
+          userId: '123'
+        },
+        body: {
+          language: 'es',
+          lengthMillis: 100,
+          questionId: 1234
+        }
+      }, lambdaContextSpy).then(() => {
+        const [err, res] = lambdaContextSpy.done.firstCall.args;
+        
+        if (err) {
+          throw err;
+        }
+        
+        const data = JSON.parse(res.body);
+        expect(res.statusCode).toBe(400);
+        expect(data.message).toBe('url is required');
+      }).then(done, done);
+    });
+    it('responds with 400 if lengthMillis not provided in post body', done => {
+      api.proxyRouter({
+        requestContext: {
+          resourcePath: '/users/{userId}/recordings',
+          httpMethod: 'POST'
+        },
+        pathParameters: {
+          userId: '123'
+        },
+        body: {
+          language: 'es',
+          url: 'www.foo.com',
+          questionId: 1234
+        }
+      }, lambdaContextSpy).then(() => {
+        const [err, res] = lambdaContextSpy.done.firstCall.args;
+        
+        if (err) {
+          throw err;
+        }
+        
+        const data = JSON.parse(res.body);
+        expect(res.statusCode).toBe(400);
+        expect(data.message).toBe('lengthMillis is required');
+      }).then(done, done);
+    });
+    it('responds with 400 if language not provided in post body', done => {
+      api.proxyRouter({
+        requestContext: {
+          resourcePath: '/users/{userId}/recordings',
+          httpMethod: 'POST'
+        },
+        pathParameters: {
+          userId: '123'
+        },
+        body: {
+          url: 'www.foo.com',
+          lengthMillis: 100,
+          questionId: 1234
+        }
+      }, lambdaContextSpy).then(() => {
+        const [err, res] = lambdaContextSpy.done.firstCall.args;
+        
+        if (err) {
+          throw err;
+        }
+        
+        const data = JSON.parse(res.body);
+        expect(res.statusCode).toBe(400);
+        expect(data.message).toBe('language is required');
+      }).then(done, done);
+    });
+    it('responds with 400 if questionId not provided in post body', done => {
+      api.proxyRouter({
+        requestContext: {
+          resourcePath: '/users/{userId}/recordings',
+          httpMethod: 'POST'
+        },
+        pathParameters: {
+          userId: '123'
+        },
+        body: {
+          url: 'www.foo.com',
+          lengthMillis: 100,
+          language: 'es'
+        }
+      }, lambdaContextSpy).then(() => {
+        const [err, res] = lambdaContextSpy.done.firstCall.args;
+        
+        if (err) {
+          throw err;
+        }
+        
+        const data = JSON.parse(res.body);
+        expect(res.statusCode).toBe(400);
+        expect(data.message).toBe('questionId is required');
+      }).then(done, done);
+    });
+    it('responds with 400 if given user does not exist', done => {
+      api.proxyRouter({
+        requestContext: {
+          resourcePath: '/users/{userId}/recordings',
+          httpMethod: 'POST'
+        },
+        pathParameters: {
+          userId: 'nonexistant'
+        },
+        body: {
+          url: 'www.foo.com',
+          lengthMillis: 100,
+          language: 'es',
+          questionId: '123'
+        }
+      }, lambdaContextSpy).then(() => {
+        const [err, res] = lambdaContextSpy.done.firstCall.args;
+        
+        if (err) {
+          throw err;
+        }
+        
+        const data = JSON.parse(res.body);
+        expect(res.statusCode).toBe(400);
+        expect(data.message).toBe('user nonexistant does not exist');
+      }).then(done, done);
+    });
+  });
+
   describe('GET /language/:languageId/questions', () => {
     it('responds with 200 and an array of questions in the specified language', done => {
       api.proxyRouter({
